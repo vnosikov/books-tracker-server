2eb107f4d423915f4d55e8ab2b16d7c5d5ae524a
Vasily Nosikov <vasily.nosikov@ararads.com>
2020-07-06 15:09:48 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys.js');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authentication')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys.js');
+
+
+const User = mongoose.model('users');
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then(existngUser => {
+      if (existngUser) {
+        done(null, existngUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(savedUser => {
+            done(null, savedUser)
+          });
+      }
+    });
+
+  },
+));
